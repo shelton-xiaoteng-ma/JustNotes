e110af814f005e15d2add6678edea9d41bf9f6d1
maxiaoteng <jizhuwo00@hotmail.com>
2024-12-28 23:02:29 +0800

    feat(clerk): integrate Clerk for authentication and user management

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>JustNotes</h1>
+      <p>JustNotes is a simple note-taking app.</p>
+      <p>This is a Landing Page.</p>
+    </div>
+  );
+}
